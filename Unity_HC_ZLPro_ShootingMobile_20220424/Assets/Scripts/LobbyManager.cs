71e953aef8a14b56f78327d7f22c69853b17ddd3
KID <50666207+KID421@users.noreply.github.com>
2022-04-24 17:05:08 +0800

    顯示連線畫面

--- Unity_HC_ZLPro_ShootingMobile_20220424/Assets/Scripts/LobbyManager.cs
+++ Unity_HC_ZLPro_ShootingMobile_20220424/Assets/Scripts/LobbyManager.cs
@@ -6,6 +6,12 @@ using UnityEngine;
 /// </summary>
 public class LobbyManager : MonoBehaviour
 {
+    // GameObject 遊戲物件：存放 Unity 場景內所有物件
+    // SerializeField 將私人欄位顯示在屬性面板上
+    // Heder 標題，在屬性面板上顯示粗體字標題
+    [SerializeField, Header("連線中畫面")]
+    private GameObject goConnectView;
+
     // 註解：說明
     // 讓按鈕跟程式溝通的流程
     // 1. 提供公開的方法 Public Method
@@ -14,5 +20,8 @@ public class LobbyManager : MonoBehaviour
     public void StartConnect()
     {
         print("開始連線...");
+
+        // 遊戲物件.啟動設定（布林值）- true 顯示，false 隱藏
+        goConnectView.SetActive(true);
     }
 }
