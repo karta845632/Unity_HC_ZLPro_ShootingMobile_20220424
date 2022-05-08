7d2518cbc411f2e4dfa26c3709be5af71b1b028a
karta845632 <104287860+karta845632@users.noreply.github.com>
2022-05-08 13:00:43 +0800

    角色方向圖示

--- Unity_HC_ZLPro_ShootingMobile_20220424/Assets/Scripts/SystemControl.cs
+++ Unity_HC_ZLPro_ShootingMobile_20220424/Assets/Scripts/SystemControl.cs
@@ -1,4 +1,5 @@
 using Photon.Pun.UtilityScripts;
+using UnityEditor.Timeline;
 using UnityEngine;
 
 //namespace 命名空間：程式區塊
@@ -14,6 +15,10 @@ namespace RKO
         private Joystick joystick;
         [SerializeField, Header("移動速度"), Range(0, 300)]
         private float speed = 3.5f;
+        [SerializeField, Header("角色方向圖示")]
+        private Transform traDirectionIcon;
+        [SerializeField, Header("角色旋轉速度"), Range(0, 100)]
+        private float speedTurn = 1.5f;
 
         private Rigidbody rig;
 
@@ -22,17 +27,18 @@ namespace RKO
             rig = GetComponent<Rigidbody>();
         }
 
-
         private void Update()
         {
-            GetJoystickValue();
-
+            // GetJoystickValue();
+            UpdateDirectionIconPos();
+            LookDirectionIcon();
         }
 
         private void FixedUpdate()
         {
             Move();
         }
+
         /// <summary>
         /// 取得虛擬搖桿值
         /// </summary>
@@ -41,13 +47,38 @@ namespace RKO
             print("<color=yellow>水平：" + joystick.Horizontal + "</color>");
         }
 
+        /// <summary>
+        /// 移動功能
+        /// </summary>
         private void Move()
         {
             //剛體.加速度 = 三維向量(Ｘ．Ｙ．Ｚ)
             rig.velocity = new Vector3(-joystick.Horizontal, 0, -joystick.Vertical ) * speed;
         }
+        /// <summary>
+        /// 更新角色方向圖示的座標
+        /// </summary>
+        private void UpdateDirectionIconPos()
+        {
+            //新座標  = 角色的座標 + 三維向量(虛擬搖桿的水平與垂直) * 方向圖示的範圍)
+            Vector3 pos =transform.position + new Vector3(-joystick.Horizontal, 0.5f, -joystick.Vertical) * 2.5f;
+            //更新方向圖示的座標 = 新座標
+            traDirectionIcon.position = pos;
+        }
 
-
+        /// <summary>
+        /// 面向方向圖示
+        /// </summary>
+        private void LookDirectionIcon()
+        {
+            //取得面向角度 = 四位元.面向角度(方向圖示 - 角色) - 方向圖示與角色的向量
+            Quaternion look = Quaternion.LookRotation(traDirectionIcon.position - transform.position);
+            //角色的角度 = 四位元.插植(角色的角度，面向角度，旋轉角度 x 一幀的時間)
+            transform.rotation = Quaternion.Lerp(transform.rotation, look, speedTurn * Time.deltaTime);
+            //角色的歐拉角度 = 三維向量(0，原本的歐拉角度Y，0)
+            transform.eulerAngles = new Vector3(0, transform.eulerAngles.y, 0);
+        
+        }
     }
 }
 
