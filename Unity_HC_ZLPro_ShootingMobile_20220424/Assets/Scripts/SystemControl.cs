c006cc5d53fe21663a6dc5fb94a8ca4819d3f568
karta845632 <104287860+karta845632@users.noreply.github.com>
2022-05-08 11:49:43 +0800

    角色控制器與移動

---
+++ Unity_HC_ZLPro_ShootingMobile_20220424/Assets/Scripts/SystemControl.cs
@@ -0,0 +1,53 @@
+using Photon.Pun.UtilityScripts;
+using UnityEngine;
+
+//namespace 命名空間：程式區塊
+namespace RKO
+{   
+    /// <summary>
+    /// 控制系統：荒野亂鬥移動功能
+    /// 虛擬搖桿控制角色移動
+    /// </summary>
+    public class SystemControl : MonoBehaviour
+    {
+        [SerializeField, Header("虛擬搖桿")]
+        private Joystick joystick;
+        [SerializeField, Header("移動速度"), Range(0, 300)]
+        private float speed = 3.5f;
+
+        private Rigidbody rig;
+
+        private void Awake()
+        {
+            rig = GetComponent<Rigidbody>();
+        }
+
+
+        private void Update()
+        {
+            GetJoystickValue();
+
+        }
+
+        private void FixedUpdate()
+        {
+            Move();
+        }
+        /// <summary>
+        /// 取得虛擬搖桿值
+        /// </summary>
+        private void GetJoystickValue()
+        {
+            print("<color=yellow>水平：" + joystick.Horizontal + "</color>");
+        }
+
+        private void Move()
+        {
+            //剛體.加速度 = 三維向量(Ｘ．Ｙ．Ｚ)
+            rig.velocity = new Vector3(-joystick.Horizontal, 0, -joystick.Vertical ) * speed;
+        }
+
+
+    }
+}
+
